379f1c791eecb148c6523c001db00b58866a31f1
Gamma23 <wei2007@gmail.com>
2022-02-06 19:48:06 +0800

    init

---
+++ dummy.js
@@ -0,0 +1,13 @@
+const name = "james";
+
+const person = {
+    firstname: 'jordan' };
+
+
+console.log(person);
+
+const sayHelloLitning = (fName) => {
+    console.log(`Hello Linting, ${fname}`)
+};
+
+sayHelloLinting('James');
